b54805eb3055feeeb99fd9dd30195b1290ea4ff5
DominoLin <x6760@outlook.com>
2019-10-29 20:18:54 +0800

    账单添加

---
+++ app/src/main/java/com/example/urbill/AddBillActivity.java
@@ -0,0 +1,14 @@
+package com.example.urbill;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bill);
+    }
+}
--- app/src/main/java/com/example/urbill/MainActivity.java
+++ app/src/main/java/com/example/urbill/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.urbill;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.robinhood.ticker.TickerUtils;
 import com.robinhood.ticker.TickerView;
 
@@ -13,10 +16,10 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
     private static final String TAG = "MainActivity";
 
 
-    private TickerView amountText;
-
-    private TextView dateText;
-    private ViewPager viewPager;
+    private TickerView amountText;//数字显示
+    private TextView dateText;//星期显示
+    private FloatingActionButton addBtn//添加账单按钮
+    private ViewPager viewPager;//账单ViewPager
 
     private int currentPagerPosition = 0;
 
@@ -25,11 +28,21 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        amountText = findViewById(R.id.amount_text);
+        amountText = findViewById(R.id.amount_text);//数字显示
         amountText.setCharacterLists(TickerUtils.provideNumberList());
-        dateText = findViewById(R.id.date_text);
+        dateText = findViewById(R.id.date_text);//星期显示
+
+        addBtn = findViewById(R.id.add_amount);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this. AddBillActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
 
         viewPager = findViewById(R.id.view_pager);
+
 
 
 
