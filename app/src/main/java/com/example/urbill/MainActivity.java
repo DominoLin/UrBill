fedcc32e79b9743fe9262fabb739bc56f4a30847
DominoLin <x6760@outlook.com>
2019-10-29 19:58:31 +0800

    MainActivity & activity_main.xml

--- app/src/main/java/com/example/urbill/MainActivity.java
+++ app/src/main/java/com/example/urbill/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.urbill;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.robinhood.ticker.TickerUtils;
+import com.robinhood.ticker.TickerView;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+    private static final String TAG = "MainActivity";
+
+
+    private TickerView amountText;
+
+    private TextView dateText;
+    private ViewPager viewPager;
+
+    private int currentPagerPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountText = findViewById(R.id.amount_text);
+        amountText.setCharacterLists(TickerUtils.provideNumberList());
+        dateText = findViewById(R.id.date_text);
+
+        viewPager = findViewById(R.id.view_pager);
+
+
+
     }
+
+
+    @Override
+    public void onPageSelected(int position) {
+        currentPagerPosition = position;
+
+    }
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
 }
